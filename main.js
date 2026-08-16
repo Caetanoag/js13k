5a3b5adc023a080a39413a366679c06df15abc47
caetano <2025311434@aluno.riogrande.ifrs.edu.br>
2026-08-16 20:57:32 -0300

    Input handling added

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 import { Renderer } from "./canvas/Renderer.js";
 import { Player } from "./entities/Player.js";
-import { Rectangle } from "./math/Rectangle.js";
 import { Vector2 } from "./math/Vector2.js";
+import { InputManager } from "./misc/InputManager.js";
 
 /**@type {CanvasRenderingContext2D} */
 const ctx = document.getElementById("canvas1").getContext('2d');
 const renderer = new Renderer(ctx);
 renderer.setSize(window.innerWidth, window.innerHeight);
-
+const inputManager = new InputManager();
 
 const playerInitialPosition = renderer.rect.center;
-const playerVelocity = new Vector2(20, 0);
-const player1 = new Player(playerInitialPosition, 50, 50, playerVelocity, renderer, {max: 10, actual:10});
+const playerVelocity = new Vector2(20, 20);
+const player1 = new Player(playerInitialPosition, 50, 50, playerVelocity, renderer, {max: 10, actual:10}, inputManager);
 
 function loop() {
   renderer.clear();
