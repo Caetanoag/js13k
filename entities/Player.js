91f48dbfd0ac3d7587b59e4ee5cd8217d7162414
caetano <2025311434@aluno.riogrande.ifrs.edu.br>
2026-08-16 20:57:21 -0300

    Input handling added

--- entities/Player.js
+++ entities/Player.js
@@ -1,12 +1,29 @@
+import { Vector2 } from "../math/Vector2.js";
 import { Entity } from "./Entity.js";
 
 export class Player extends Entity {
-  constructor(position, width, height, velocity, renderer, hp) {
+  constructor(position, width, height, velocity, renderer, hp, inputManager) {
     super(position, width, height, velocity, renderer);
     this.hp = hp;
+    this.inputManager = inputManager;
   }
   collide() {
 
+  }
+  handleInputs() {
+    this.velocity = Vector2.zero();
+    if (this.inputManager.isKeyDown("ArrowRight")) {
+      this.velocity.x = this.baseVelocity.x;
+    }
+    if (this.inputManager.isKeyDown("ArrowLeft")) {
+      this.velocity.x = -this.baseVelocity.x;
+    }
+    if (this.inputManager.isKeyDown("ArrowUp")) {
+      this.velocity.y = -this.baseVelocity.y;
+    }
+    if (this.inputManager.isKeyDown("ArrowDown")) {
+      this.velocity.y = this.baseVelocity.y;
+    }
   }
   move() {
     super.move();
@@ -15,6 +32,7 @@ export class Player extends Entity {
     this.renderer.fillRectangle(this, "red");
   }
   update() {
+    this.handleInputs()
     this.move();
     if (!this.isInside(this.renderer.rect)) {
       this.position.subtract(this.velocity);
