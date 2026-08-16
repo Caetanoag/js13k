60615668f7beee75b2838cad88119cf10230f886
caetano <2025311434@aluno.riogrande.ifrs.edu.br>
2026-08-16 20:33:05 -0300

    New Getters top, bottom, left and right. New boolean methods intersects and isInside

--- math/Rectangle.js
+++ math/Rectangle.js
@@ -6,11 +6,41 @@ export class Rectangle {
     this.width = width;
     this.height = height;
   }
+  get left() {
+    return this.position.x;
+  }
+  get right() {
+    return this.position.x + this.width;
+  }
+  get top() {
+    return this.position.y;
+  }
+  get bottom() {
+    return this.position.y + this.height;
+  }
   get center() {
-    const { x, y } = this.position;
-    return new Vector2(x + this.width / 2, y + this.height / 2);
+    return new Vector2(
+      this.right - this.width / 2,
+      this.bottom - this.height / 2,
+    );
   }
   scale(sx, sy) {
     return new Rectangle(this.position, this.width * sx, this.height * sy);
+  }
+  intersects(other) {
+    return (
+      this.left <= other.right &&
+      other.left <= this.right &&
+      this.top <= other.bottom &&
+      other.top <= this.bottom
+    );
+  }
+  isInside(other) {
+    return (
+      this.left >= other.left &&
+      this.right <= other.right &&
+      this.top >= other.top &&
+      this.bottom <= other.bottom
+    );
   }
 }
