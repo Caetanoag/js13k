41beaa1aaf43c23b8deca87ea7735efa11239972
caetano <2025311434@aluno.riogrande.ifrs.edu.br>
2026-08-16 20:33:43 -0300

    New class Entity

---
+++ entities/Entity.js
@@ -0,0 +1,14 @@
+import { Rectangle } from "../math/Rectangle.js";
+
+export class Entity extends Rectangle {
+  constructor(position, width, height, velocity, renderer) {
+    super(position, width, height);
+    this.velocity = velocity;
+    this.renderer = renderer;
+  }
+  move() {
+    this.position.add(this.velocity);
+  }
+  draw() {}
+  update() {}
+}
