a4f30cc9a09b612deb782c51457f48696872c7fd
caetano <2025311434@aluno.riogrande.ifrs.edu.br>
2026-08-16 21:42:12 -0300

    Entities global array and shooting player

--- main.js
+++ main.js
@@ -9,14 +9,18 @@ const renderer = new Renderer(ctx);
 renderer.setSize(window.innerWidth, window.innerHeight);
 const inputManager = new InputManager();
 
+const entities = [];
+
 const playerInitialPosition = renderer.rect.center;
 const playerVelocity = new Vector2(20, 20);
-const player1 = new Player(playerInitialPosition, 50, 50, playerVelocity, renderer, {max: 10, actual:10}, inputManager);
+const player1 = new Player(playerInitialPosition, 50, 50, playerVelocity, renderer, entities, {max: 10, actual:10}, inputManager);
 
+entities.push(player1);
 function loop() {
   renderer.clear();
   player1.update();
   player1.draw();
+  inputManager.update();
   requestAnimationFrame(loop);
 }
 requestAnimationFrame(loop)
