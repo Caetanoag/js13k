d5fc7683d6b415b79f26b5d925f7799bebab5088
caetano <2025311434@aluno.riogrande.ifrs.edu.br>
2026-08-16 21:40:49 -0300

    All Methods now return this or new Vector2. New methods equals, clone, normalized and scale. New getter length

--- math/Vector2.js
+++ math/Vector2.js
@@ -6,16 +6,37 @@ export class Vector2 {
   static zero() {
     return new Vector2(0, 0);
   }
+  get length() {
+    return Math.hypot(this.x, this.y);
+  }
   add(v) {
     this.x += v.x;
     this.y += v.y;
+    return this;
   }
   subtract(v) {
     this.x -= v.x;
     this.y -= v.y;
+    return this;
   }
   negate() {
     this.x = -this.x;
     this.y = -this.y;
+    return this;
+  }
+  scale(sx, sy) {
+    this.x *= sx;
+    this.y *= sy;
+    return this;
+  }
+  normalized() {
+    if (this.length === 0) return Vector2.zero();
+    return new Vector2(this.x / this.length, this.y / this.length);
+  }
+  clone() {
+    return new Vector2(this.x, this.y);
+  }
+  equals(v) {
+    return this.x === v.x && this.y === v.y;
   }
 }
