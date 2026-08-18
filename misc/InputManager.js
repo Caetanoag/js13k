2c0788b2def45cde37e338913969cdb48fbd6483
caetano <2025311434@aluno.riogrande.ifrs.edu.br>
2026-08-18 17:12:56 -0300

    Added mouse support

--- misc/InputManager.js
+++ misc/InputManager.js
@@ -1,7 +1,14 @@
+import { Vector2 } from "../math/Vector2.js";
+
 export class InputManager {
-  constructor() {
+  constructor(target) {
+    this.target = target;
     this.keysDown = new Map();
     this.keysPressed = new Map();
+    this.mouse = {
+      position: Vector2.zero(),
+      buttons: new Map(), // <int, bool>
+    };
     window.addEventListener("keydown", (e) => {
       const key = e.key;
       if (!this.keysDown.get(key)) this.keysPressed.set(key, true);
@@ -10,8 +17,29 @@ export class InputManager {
     window.addEventListener("keyup", (e) => {
       this.keysDown.set(e.key, false);
     });
+    this.target.addEventListener("mousemove", (e) => {
+      const rect = this.target.getBoundingClientRect();
+      this.mouse.position = new Vector2(
+        e.clientX - rect.left,
+        e.clientY - rect.top,
+      );
+    });
+    this.target.addEventListener("mousedown", (e) => {
+      this.mouse.buttons.set(e.button, true);
+      e.preventDefault();
+    });
+    this.target.addEventListener("mouseup", (e) => {
+      this.mouse.buttons.set(e.button, false);
+      e.preventDefault();
+    });
   }
-  isKeyDown(key) { return this.keysDown.get(key) ?? false; }
-  isKeyPressed(key) { return this.keysPressed.get(key) ?? false; }
-  update() { this.keysPressed.clear(); }
+  isKeyDown(key) {
+    return this.keysDown.get(key) ?? false;
+  }
+  isKeyPressed(key) {
+    return this.keysPressed.get(key) ?? false;
+  }
+  update() {
+    this.keysPressed.clear();
+  }
 }
