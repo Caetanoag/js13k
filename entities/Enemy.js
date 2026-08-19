8964f35139863f2c7d5ca4404ff6871cd4fb8dda
caetano <2025311434@aluno.riogrande.ifrs.edu.br>
2026-08-19 19:27:53 -0300

    Enemy types

--- entities/Enemy.js
+++ entities/Enemy.js
@@ -1,9 +1,22 @@
 import { Entity } from "./Entity.js";
+import { Vector2 } from "../math/Vector2.js";
 
 export class Enemy extends Entity {
-  constructor(position, width, height, velocity, renderer, entities, hp) {
+  constructor(
+    position,
+    width,
+    height,
+    velocity,
+    renderer,
+    entities,
+    hp,
+    damage,
+    color,
+  ) {
     super(position, width, height, velocity, renderer, entities);
     this.hp = hp;
+    this.damage = damage;
+    this.color = color;
   }
   followPlayer(player) {
     this.target = player;
@@ -14,7 +27,7 @@ export class Enemy extends Entity {
     this.velocity = direction.scale(this.baseVelocity.x, this.baseVelocity.y);
   }
   draw() {
-    this.renderer.fillRectangle(this, "blue");
+    this.renderer.fillRectangle(this, this.color);
   }
   move() {
     super.move();
@@ -22,7 +35,42 @@ export class Enemy extends Entity {
   update() {
     this.move();
     if (this.intersects(this.target)) {
-      this.target.hp.actual -= 1;
+      this.target.hp.actual -= this.damage;
     }
+  }
+}
+export class TankEnemy extends Enemy {
+  constructor(position, width, height, renderer, entities) {
+    const velocity = new Vector2(3, 3);
+    const hp = { max: 5, actual: 5 };
+    super(
+      position,
+      width,
+      height,
+      velocity,
+      renderer,
+      entities,
+      hp,
+      2,
+      "green",
+    );
+  }
+}
+
+export class FastEnemy extends Enemy {
+  constructor(position, width, height, renderer, entities) {
+    const velocity = new Vector2(8, 8);
+    const hp = { max: 2, actual: 2 };
+    super(
+      position,
+      width,
+      height,
+      velocity,
+      renderer,
+      entities,
+      hp,
+      1,
+      "yellow",
+    );
   }
 }
