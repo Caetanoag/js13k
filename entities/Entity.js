e7106d9e377849abef0016c7b99e4cff8820780e
caetano <2025311434@aluno.riogrande.ifrs.edu.br>
2026-08-20 20:26:07 -0300

    Implemented hp bar drawing

--- entities/Entity.js
+++ entities/Entity.js
@@ -1,4 +1,5 @@
 import { Rectangle } from "../math/Rectangle.js";
+import { Vector2 } from "../math/Vector2.js";
 
 export class Entity extends Rectangle {
   constructor(position, width, height, velocity, renderer, entities) {
@@ -11,6 +12,15 @@ export class Entity extends Rectangle {
   move() {
     this.position.add(this.velocity);
   }
-  draw() {}
+  drawHpBar() {
+    const barCenter = this.center.add(new Vector2(0, -this.height/1.2));
+    const bar = Rectangle.fromCenter(barCenter, this.width*1.3, this.height * 0.2);
+    this.renderer.fillRectangle(bar, "black");
+    this.renderer.fillRectangle(bar.scale(Math.max(0, this.hp.actual / this.hp.max), 1), "green");
+
+  }
+  draw() {
+    if (this.hp) this.drawHpBar();
+  }
   update() {}
 }
